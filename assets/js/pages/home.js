35be23395265a6b2253b01e914bc3aaf60800f45
maxdeschamps <zemoax@hotmail.fr>
2020-01-29 20:42:29 +0100

    ajout du compteur js sur la homepage

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -1,11 +1,27 @@
-$('.count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 4000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
+$( document ).ready(function() {
+  var counterOn = true;
+
+  function isScrolledIntoView(elem) {
+      var docViewTop = $(window).scrollTop();
+      var docViewBottom = docViewTop + $(window).height();
+      var elemTop = $(elem).offset().top;
+      return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
+  }
+
+  $(window).scroll(function() {
+    if (isScrolledIntoView($('.count')) && counterOn) {
+      counterOn = false;
+      $('.count').each(function () {
+        $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+        }, {
+          duration: 4000,
+          easing: 'swing',
+          step: function (now) {
+            $(this).text(Math.ceil(now) + ' %');
+          }
+        });
+      });
+    }
+  });
 });
