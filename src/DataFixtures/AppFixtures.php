757043e19482c5cb42582e501221ccb03a567553
Meganoveur <antoine1.chatard8@gmail.com>
2020-02-03 11:23:51 +0100

    fixture new version

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,9 +9,10 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
-        $manager->flush();
+        $manyProductFixtures = new ManyProductFixtures();
+
+        $manyProductFixtures->load($manager);
+
     }
 }
--- src/DataFixtures/ManyProductFixtures.php
+++ src/DataFixtures/ManyProductFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Address;
+use App\Entity\Category;
+use App\Entity\Role;
 use App\Entity\User;
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -13,18 +16,59 @@ class ManyProductFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Faker\Factory::create('fr_FR');
-        $populator = new \Faker\ORM\Propel\Populator($faker);
-        
+
         for ($i = 0; $i < 50; $i++) {
-            $produit = new Product();
-            $produit->addEntity('User', 1);
-            $produit->setName($faker->word(7));
-            $produit->setSlug($faker->word(10));
-            $produit->setContent($faker->text);
-            $produit->setPriceHt($faker->numberBetween($min = 1000, $max = 30000));
-            $produit->setPriceTtc($faker->numberBetween($min = 1000, $max = 30000));
-            $produit->setQuantity($faker->numberBetween(0,50));
-            $manager->persist($produit);
+            $category = new Category();
+            $category->setName($faker->word(7));
+            $category->setSlug($faker->word(10));
+
+            $manager->persist($category);
+
+            for ($i = 0; $i < 4; $i++) {
+                $role = new Role();
+                $role->setName($faker->word(7));
+                $role->setSlug($faker->word(10));
+
+                $manager->persist($role);
+            }
+
+            for ($i = 0; $i < 50; $i++) {
+                $address = new Address();
+                $address->setLine1($faker->address);
+                $address->setLine2($faker->address);
+                $address->setPostalCode($faker->postcode);
+                $address->setCity($faker->city);
+                $address->setCountry($faker->country);
+
+                $manager->persist($address);
+            }
+
+            for ($i = 0; $i < 50; $i++) {
+                $personne = new User();
+                $personne->setFirstName($faker->firstName);
+                $personne->setLastName($faker->lastName);
+                $personne->setPhone($faker->phoneNumber);
+                $personne->setPassword($faker->password);
+                $personne->setEmail($faker->email);
+                $personne->setNewsletterAcceptance(intval($faker->boolean));
+
+                $manager->persist($personne);
+            }
+
+            for ($i = 0; $i < 50; $i++) {
+                $produit = new Product();
+                $produit->setCategory($category);
+                $produit->setAuthor($personne);
+                $produit->setName($faker->word(7));
+                $produit->setSlug($faker->word(10));
+                $produit->setContent($faker->text);
+                $produit->setPriceHt($faker->numberBetween($min = 1000, $max = 30000));
+                $produit->setPriceTtc($faker->numberBetween($min = 1000, $max = 30000));
+                $produit->setQuantity($faker->numberBetween(0, 50));
+
+                $manager->persist($produit);
+
+            }
         }
         $manager->flush();
     }
