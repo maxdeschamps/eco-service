95fed0c00a390f0f82c5a9375f90a1efe99fd962
Antoine <Chatard>
2020-02-19 11:02:14 +0100

    dashboard

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Bill;
+use App\Repository\BillRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
 
@@ -10,10 +13,14 @@ class AdminController extends BaseAdminController
     /**
      * @Route("/dashboard", name="admin_dashboard")
      */
-    public function index()
+    public function index(EntityManagerInterface $em)
     {
+        $repository = $em->getRepository(Bill::class);
+        $bills = $repository->findAll();
+
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'bills' => $bills
         ]);
     }
 }
