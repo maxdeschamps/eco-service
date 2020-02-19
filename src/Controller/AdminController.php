eb4c117931d6d2445e9a9e057988ffb3ae396852
Antoine <Chatard>
2020-02-19 11:57:57 +0100

    dashboard

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Bill;
+use App\Entity\Message;
+use App\Entity\Product;
+use App\Entity\User;
 use App\Repository\BillRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -17,10 +20,27 @@ class AdminController extends BaseAdminController
     {
         $repository = $em->getRepository(Bill::class);
         $bills = $repository->findAll();
+        $countBills=count($bills);
+
+
+        $repository = $em->getRepository(Product::class);
+        $products = $repository->findAll();
+        $countProducts=count($products);
+
+        $repository = $em->getRepository(User::class);
+        $users = $repository->findAll();
+        $countUsers=count($users);
+
+        $repository = $em->getRepository(Message::class);
+        $messages = $repository->findAll();
+        $countMessages=count($messages);
 
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
-            'bills' => $bills
+            'countBills' => $countBills,
+            'countProducts' => $countProducts,
+            'countUsers' => $countUsers,
+            'countMessages' => $countMessages
         ]);
     }
 }
