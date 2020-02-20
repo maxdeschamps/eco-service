ca612b72ff2ba9dd20ce150cb7120aad4caebbe3
Antoine <Chatard>
2020-02-20 10:56:38 +0100

    modification titre + typage des champs edition + textarea WYSWIG

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
