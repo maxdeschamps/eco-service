2ce7f0b61e7c5dc3ff8aa21f353c8d7e29dc58fd
Lisa <lil0ubiscuit@hotmail.com>
2020-03-09 17:29:40 +0100

    formulaire contact fini

--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -3,18 +3,41 @@
 namespace App\Form;
 
 use App\Entity\Message;
+use App\Entity\Subject;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+
 
 class MessageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content')
-            ->add('subject')
-            ->add('author')
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu'])
+            ->add('subject', EntityType::class, [
+                'class' => Subject::class,
+                'label' => 'Sujet'])
+            ->add('email', EmailType::class, [
+                'label' => 'Email'])
+            ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'En cochant cette case, vous acceptez nos conditions d\'utilisation',
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter nos conditions',
+                    ]),
+                ],
+            ])
         ;
     }
 
